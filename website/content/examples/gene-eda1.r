47a9d95d0d2cbabd8bb46ff7bc85fe0c25aff563
Jarrod Millman <jarrod.millman@gmail.com>
2014-06-20 08:42:32 -0700

    Improve example EDA

--- website/content/examples/gene-eda1.r
+++ website/content/examples/gene-eda1.r
@@ -7,7 +7,7 @@ class(gse349Info)
 head(gse349)
 head(gse349Info)
 
-names(gse349) # what's wrong with this?
+names(gse349)        # what happened here?
 names(gse349Info)
 
 str(gse349)
@@ -15,7 +15,6 @@ dimnames(gse349)
 class(dimnames(gse349))
 dimnames(gse349)[2]
 dimnames(gse349)[[2]]
-
 
 dim(gse349)
 summary(gse349)
@@ -40,17 +39,27 @@ sum(x>7)
 sum(x<7)/length(x)
 hist(log(x[x>7]))
 
-apply(dse349, 2,  mean)
+apply(gse349, 2,  mean)
 apply(gse349, 2,  median)
 apply(gse349, 2,  quantile, probs=0.5)
 apply(gse349, 2,  quantile, probs=0.75)
 
-cutoff = apply(gse349, 2,  quantile, probs=0.1)
+cutoff = quantile(gse349, probs=0.1)
+#cutoff = apply(gse349, 2,  quantile, probs=0.1)
+filtered.exprs = gse349[gse349>cutoff]
+x = filtered.exprs[,1]
+summary(x)
+sum(x<7)
+sum(x>7)
+sum(x<7)/length(x)
+hist(log(x[x>7]))
 
+log.filtered.exprs = log(filtered.exprs)
+boxplot(log.filtered.exprs)
 
 status=factor(gse349Info$status)
 table(status)
 
 cols = c("red","blue")
-boxplot(gse349, col=cols[status])
+boxplot(log.filtered.exprs, col=cols[status])
 legend("topleft", levels(status), fill=cols)

f932a25c7df5e9cac5430cd7390bbed4ee3db0d5
Jarrod Millman <jarrod.millman@gmail.com>
2014-06-22 19:28:50 -0700

    Add median normalization

--- website/content/examples/quantile-normalization.r
+++ website/content/examples/quantile-normalization.r
@@ -2,10 +2,22 @@ quantile.norm = function(x) {
     # x: p by n data matrix
     #    where columns are the samples and rows are observations
 
-    norm = x
     x.sort = apply(x, 2, sort)    # sort within sample
     x.rank = apply(x, 2, rank)    # rank within sample
     reference = rowMeans(x.sort)
     norm = apply(x.rank, 2, function(smpl) reference[smpl])
+    dimnames(norm) = dimnames(x)
+    return(norm)
+}
+
+median.norm = function(x) {
+    # x: p by n data matrix
+    #    where columns are the samples and rows are observations
+
+    medians = apply(x, 2, median)
+    reference = mean(medians)
+    d = reference - medians
+    norm = sapply(1:14,  function(i) x[,i]+d[i])
+    dimnames(norm) = dimnames(x)
     return(norm)
 }
